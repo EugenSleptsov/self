94d56e139e693911b8abc86bbd9071fa001f985b
Evgeniy Sleptsov <slepcov@unitiki.com>
2024-04-08 15:19:16 +0300

    Next iteration

--- _exec.sh
+++ _exec.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Attempt to start the Go program
 go run .
 
-# Check the exit code of the program
 if [ $? -ne 0 ]; then
-    # If program failed to start, reset to the last commit
-    git reset --hard HEAD^
+    git reset --hard
+    git clean -fd
 else
-    # If program started successfully, commit changes and push to the server
     git add .
     git commit -m "Next iteration"
     git push origin master
