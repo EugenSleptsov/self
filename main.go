ca1a0676b417b66fbafa9c45c4e674f8db9790c8
Evgeniy Sleptsov <slepcov@unitiki.com>
2024-04-08 15:52:29 +0300

    self writing

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	content := ``
+
+	file, err := os.OpenFile("main.go", os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+	file.WriteString(content)
 }
