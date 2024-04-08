920001d675783b7ca9c3f527ebf1b86e2100edb5
Evgeniy Sleptsov <slepcov@unitiki.com>
2024-04-08 15:48:29 +0300

    Next iteration of the project, time: 2024-04-08 15:48:29

--- _exec.sh
+++ _exec.sh
@@ -7,6 +7,6 @@ if [ $? -ne 0 ]; then
     git clean -fd
 else
     git add .
-    git commit -m "Next iteration of the project, time: $(date)"
+    git commit -m "Next iteration of the project, time: $(date +'%Y-%m-%d %H:%M:%S')"
     git push
 fi
